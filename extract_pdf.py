5756a0c35ab1277a187d9030e536b555768a5102
Castielbits <castielbits@gmail.com>
2026-03-31 13:29:42 -0300

    Refina .gitignore e limpa arquivos do repositório

--- extract_pdf.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("Copy Dra. Fernanda (2).pdf") as pdf:
-    for page in pdf.pages:
-        text = page.extract_text()
-        if text:
-            print(text)
